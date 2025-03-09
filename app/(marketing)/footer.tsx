6d0cc5e6dcd340dd39e76dcb192ebbe680317225
BhanukaAGH <abewickramah1@gmail.com>
2025-03-09 12:04:58 +0530

    feat: enhance marketing page with advanced animations and interactive sections

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
-    <footer className='py-4'>
+    <footer className='border-t border-gray-200 py-10'>
       <div className='container text-center text-sm text-muted-foreground'>
         <p>
-          RecruitMind &copy;{new Date().getFullYear()}. All rights reserved.
+          &copy; {new Date().getFullYear()} RecruitMind. All rights reserved.
         </p>
       </div>
     </footer>
